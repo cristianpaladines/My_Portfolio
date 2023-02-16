431c62f9bf41ac71d216a9a0f97a84495d2cb110
cristianpaladines <cristianpaladines21@gmail.com>
2023-02-16 16:43:13 -0500

    se agregan arreglos en el home.html y los test en los dos archivos test.py de portafolio y blog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+class SegundoTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title="ejemplo", description="ejemplo de descripcion", image="imagen.jpg", date="16/02/2023")
+        
+    def test_modelo(self):
+        modelo = Post.objects.get(title="ejemplo")
+        self.assertEqual(modelo.description, "ejemplo de descripcion")
--- portafolio/tests.py
+++ portafolio/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Project
+
+class PrimerTest(TestCase):
+    def setUp(self):
+        Project.objects.create(title="ejemplo", description="ejemplo de descripcion", image="imagen.jpg", url="http://example.com")
+        
+    def test_modelo(self):
+        modelo = Project.objects.get(title="ejemplo")
+        self.assertEqual(modelo.description, "ejemplo de descripcion")
+
